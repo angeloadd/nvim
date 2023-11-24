dfa05ec792137d49d2f22eb60977d87acd97d7e5
Angelo <angelo.adduci1991@gmail.com>
2023-11-24 23:00:52 +0100

    Add keymaps and autocmd

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local function map(mode, key, remap, description, opts)
+  opts.desc = description
+  vim.keymap.set(mode, key, remap, opts)
+end
+
+local function nmap(key, remap, description)
+  map("n", key, remap, description, {})
+end
+
+nmap("x", '"_x', "Delete without copying to clipboard and vim buffer")
+
+nmap("<C-a>", "gg<S-v>G", { desc = "Select All" })
+
+--keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+--vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- When doing page up and page down center selectiotion
+nmap("<C-d>", "<C-d>zz", "PageUp and center cursor")
+nmap("<C-u>", "<C-u>zz", "PageDown and center cursor")
+-- When going to ext search or previous search center the selection
+nmap("N", "Nzzzv", "Go to previous result in search and cente page on it")
+
+map({ "n", "v" }, "<leader>d", [["_d]], "Delete without yanking", {})
+
+nmap("<leader>si", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,84 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.mapleader = " "
+
+vim.opt.guicursor = ""
+
+-- Enable numbers in buffer and enable relative numbers this is set only for show as LazyVim already enables this
+-- options.
+-- vim.opt.nu = true
+-- vim.opt.relativenumber = true
+
+-- Tab takes for spaces everywhere, automatically and when you press tab, on top of this it expands tabs to 4 spaces
+-- for encoding purposes I think.
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+-- auto indent files, by copying previous line indent (autoindent) and then tries to use language to do a smartindent
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.breakindent = true
+-- do not wrap lines, show them going in the sun (after scroll bar)
+vim.opt.wrap = false
+
+-- no backup or swap, those annoying file vim creates when it crashes or you don't save stuff correctly
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- Disable highlighting whilw you search but does incremental search, basically while you type it shows stuff
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Enable true colors where it can
+vim.opt.termguicolors = true
+
+-- Shows always 8 column top or bottom to not reach the end of the file
+vim.opt.scrolloff = 8
+
+-- this is a valid carachter to use in file name
+vim.opt.isfname:append("@-@")
+
+-- This is difficult so basically when the cursor stays idle after sometimes Vim triggers an event that can be used.
+vim.opt.updatetime = 50
+
+-- shoe the column for format purposes
+vim.opt.colorcolumn = "120"
+-- this option works in parallel with the debugger by enabling signs in case there are to show
+vim.opt.signcolumn = "yes"
+
+-- set encoding, uuuuh
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
+
+-- tries to show the name of the file as the title of the terminal tab nvim tab
+vim.opt.title = true
+
+-- option regarding the line shows the command and it is always shown
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+
+-- use zsh as shell for running command that are external
+vim.opt.shell = "zsh"
+
+-- Case insensitive searching UNLESS /C or capital in search
+vim.opt.ignorecase = true
+
+-- vim.opt.backspace = { "start", "eol", "indent" }
+vim.opt.path:append({ "**" }) -- Finding files - Search down into subfolders
+vim.opt.wildignore:append({ "*/node_modules/*" })
+--vim.opt.splitbelow = true -- Put new windows below current
+--vim.opt.splitright = true -- Put new windows right of current
+--vim.opt.splitkeep = "cursor"
+vim.opt.mouse = ""
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Add asterisks in block comments or whatever othe block comment means exists. For example in lua it keeps addin `--`
+vim.opt.formatoptions:append({ "r" })
